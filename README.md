6a08f8a178ec1695279e435ebafff629fab13b27
zhangYuan <zhangyuan@authing.cn>
2023-02-13 16:52:00 +0800

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Introduction 工程简介
+
+An example integration scenario that used the starter for using Redis key-value data store with Spring Data Redis and the Lettuce client.
+
+# Extended Reading 延伸阅读
+
+Spring-Data-Redis Documentation:
+
+https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/#redis:connectors:connection
---
+++ src/main/java/com/erato/springdata/keyvalue/example/SpringDataKeyvalueExampleApplication.java
@@ -0,0 +1,13 @@
+package com.erato.springdata.keyvalue.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataKeyvalueExampleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataKeyvalueExampleApplication.class, args);
+    }
+    
+}
